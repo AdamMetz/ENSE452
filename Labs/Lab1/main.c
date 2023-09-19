471b70d28f5817e80ce188bf6afd57f20fdb2744
Adam Metz <alm939@uregina.ca>
2023-09-19 16:42:13 -0600

    Added blinking functionality

--- Labs/Lab1/main.c
+++ Labs/Lab1/main.c
@@ -1,5 +1,12 @@
 #include "stm32f10x.h"
 
+// Delay length of 7 200 000 for ~1s delay, (72 MHz clock)
+#define DELAY_LENGTH 7200000
+
+void delay(int ticks) {
+    while (ticks--);
+}
+
 int main(void) {
 	
     // Enable Clock
@@ -9,10 +16,10 @@ int main(void) {
     GPIOA->CRL &= ~(GPIO_CRL_MODE5 | GPIO_CRL_CNF5);
     GPIOA->CRL |= GPIO_CRL_MODE5_0;
 
-    // Turn on PA5
-    GPIOA->BSRR |= GPIO_BSRR_BS5;
-
     while (1) {
-
+			GPIOA->BSRR |= GPIO_BSRR_BS5;
+			delay(DELAY_LENGTH);
+			GPIOA->BSRR |= GPIO_BSRR_BR5;
+			delay(DELAY_LENGTH);
     }
 }
